1cdf6e0f92645189c1fa9e2849088744489bdc37
Rilavalon <rilavalon05@gmail.com>
2016-06-05 02:32:34 +0200

    adding working crypto functions

--- projet/ssl_test.c
+++ projet/ssl_test.c
@@ -1,10 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <openssl/rsa.h>
-#include <openssl/evp.h>
-#include <openssl/err.h>
 #include <openssl/rand.h>
+#include <openssl/des.h>
+#include <openssl/evp.h>
 
 //pour générer un sel
 void generateChallenge(unsigned char *challenge,int chl_size){
@@ -21,72 +20,109 @@ void generateChallenge(unsigned char *challenge,int chl_size){
 }
 
 //fonction pour chiffrer/déchiffrer
-int do_crypt (char *infile, char *outfile, int do_encrypt)
+int crypto(int mode)
     {
-    	printf("in crypto\n");
+        char *key_data="Password";
 
         /* Allow enough space in output buffer for additional block */
-        unsigned char inbuf[2048], outbuf[1024 + EVP_MAX_BLOCK_LENGTH],
+        unsigned char inbuf[1024], outbuf[1024 + EVP_MAX_BLOCK_LENGTH],
                       key[32], iv[32], salt[8];
 
-        char *key_data="monMonDePasse";
         int key_data_len = strlen(key_data), nrounds = 1, inlen, outlen;
+        if (mode!=1)
+        {
+           FILE *in = fopen("text.txt","rb");
+            FILE *out = fopen("text.e.txt","wb");
 
-        FILE *in = fopen(infile,"r");
-        FILE *out = fopen(outfile,"wa");
- 		printf("files open\n");
-        if(do_encrypt){ //if encryption case
             generateChallenge(salt,8);
             fwrite(salt, 1, 8, out);
-        }
-        else{ //if decryption case
-            fread(salt, 1, 8, in);
-        }
- 		printf("begin\n");
-        //derivate key & iv from the supplied password
-        EVP_BytesToKey(EVP_aes_256_cbc(), EVP_md5(), salt, (unsigned char*)key_data, key_data_len, nrounds, key, iv);
 
-        EVP_CIPHER_CTX ctx;
-        EVP_CIPHER_CTX_init(&ctx);
-        EVP_CipherInit_ex(&ctx, EVP_aes_256_cbc(), NULL, key, iv, do_encrypt);
- 		printf("key ok\n");
-        for(;1;)
-        {
-            printf("key ok %s \n\n %s lambda\n",key, salt);
-            inlen = fread(inbuf, 1, 1, in);
-            printf("key ok2\n");
-            if(inlen <= 0) break;
-            printf("key ok3\n");
-            if(!EVP_CipherUpdate(&ctx, outbuf, &outlen, inbuf, inlen))
+            //derivate key & iv from the supplied password
+            EVP_BytesToKey(EVP_aes_256_cbc(), EVP_md5(), salt, (unsigned char*)key_data, key_data_len, nrounds, key, iv);
+
+            EVP_CIPHER_CTX ctx;
+            EVP_CIPHER_CTX_init(&ctx);
+            EVP_CipherInit_ex(&ctx, EVP_aes_256_cbc(), NULL, key, iv,1);
+
+            for(;;)
+            {
+             inlen = fread(inbuf, 1, 1024, in);
+                if(inlen <= 0) break;
+                if(!EVP_CipherUpdate(&ctx, outbuf, &outlen, inbuf, inlen))
+                {
+                    /* Error */
+                    EVP_CIPHER_CTX_cleanup(&ctx);
+                    return 0;
+                }
+                fwrite(outbuf, 1, outlen, out);
+            }
+
+            if(!EVP_CipherFinal_ex(&ctx, outbuf, &outlen))
             {
                 /* Error */
                 EVP_CIPHER_CTX_cleanup(&ctx);
                 return 0;
             }
-            printf("key ok4\n");
-            fwrite(outbuf, 1, outlen, out);
-            printf("key ok5\n");
-        }
- 		printf("first for\n");
-        if(!EVP_CipherFinal_ex(&ctx, outbuf, &outlen))
-        {
-            /* Error */
-            EVP_CIPHER_CTX_cleanup(&ctx);
-            return 0;
-        }
 
-        fwrite(outbuf, 1, outlen, out);
-        EVP_CIPHER_CTX_cleanup(&ctx);
- 		printf("before closing\n");
-        fclose(in);
-        fclose(out);
+            fwrite(outbuf, 1, outlen, out);
+            EVP_CIPHER_CTX_cleanup(&ctx);
+
+            fclose(in);
+            fclose(out); 
+        }
+        else
+        {
+            FILE *in2 = fopen("text.e.txt","rb");
+            FILE *out2 = fopen("text.d.txt","wb");
+
+            fread(salt, 1, 8, in2);
+
+            //derivate key & iv from the supplied password
+            EVP_BytesToKey(EVP_aes_256_cbc(), EVP_md5(), salt, (unsigned char*)key_data, key_data_len, nrounds, key, iv);
+
+            EVP_CIPHER_CTX ctx2;
+            EVP_CIPHER_CTX_init(&ctx2);
+            EVP_CipherInit_ex(&ctx2, EVP_aes_256_cbc(), NULL, key, iv, 0);
+
+            for(;;)
+            {
+                inlen = fread(inbuf, 1, 1024, in2);
+                if(inlen <= 0) break;
+                if(!EVP_CipherUpdate(&ctx2, outbuf, &outlen, inbuf, inlen))
+                {
+                    /* Error */
+                    EVP_CIPHER_CTX_cleanup(&ctx2);
+                    return 0;
+                }
+                fwrite(outbuf, 1, outlen, out2);
+            }
+
+            if(!EVP_CipherFinal_ex(&ctx2, outbuf, &outlen))
+            {
+                /* Error */
+                EVP_CIPHER_CTX_cleanup(&ctx2);
+                return 0;
+            }
+
+            fwrite(outbuf, 1, outlen, out2);
+            EVP_CIPHER_CTX_cleanup(&ctx2);
+
+            fclose(in2);
+            fclose(out2);
+        }
 
         return 1;
     }
 
-    int main(void){
-        printf("salut\n");
-        do_crypt("test.txt","test.txt.e",1); //encryption
-        do_crypt("test.txt.e","test.txt.d",0); //decryption
-        return 0;
-    }
+int main(void)
+{
+    int mode;
+    //char* password[50];
+    printf("Entrez un chiffre : pour le cryptage => 0 ou  pour le decryptage => 1\n");
+    scanf("%d",&mode);
+    //printf("\nEntrez un password: \n");
+    //scanf("%s",&password);
+    crypto(mode);
+    return 0;
+}
+
